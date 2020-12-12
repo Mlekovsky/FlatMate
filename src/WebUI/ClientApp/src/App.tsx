2de81697934ef1b01a2f82ff543493d1ffe8f5de
Damian <damian.smietana97@gmail.com>
2020-12-12 20:35:14 +0100

    Starting work with login page - refactor - tsx

---
+++ src/WebUI/ClientApp/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import './custom.css'
+import { Login } from './components/Login/Login';
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </Layout>
+    );
+  }
+}
